76d3707bd8fc3391598fb5309701deb558c37139
Lucy Lu Wang <lucee25@gmail.com>
2018-10-24 18:47:09 -0700

    added paths

--- syngsea/paths.py
+++ syngsea/paths.py
@@ -5,6 +5,7 @@ INIT_DIR = '/Users/lwang/git/synthetic_gsea'
 class SynGSEAFilePath(object):
     syngsea_source_folder = 'syngsea'
     syngsea_output_folder = 'output'
+    gsea_folder = 'GSEA'
 
     def __init__(self, base_dir=INIT_DIR):
         """Set self.base_dir.
@@ -15,4 +16,10 @@ class SynGSEAFilePath(object):
     def output_dir(self):
         return os.path.join(
             self.base_dir, self.syngsea_output_folder
+        )
+
+    @property
+    def gsea_dir(self):
+        return os.path.join(
+            self.base_dir, self.gsea_folder
         )
